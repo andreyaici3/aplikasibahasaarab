1e19d3b0d6e552099a129863fe49c2f296ec18d2
Andrey Andriansyah <andreyandri90@gmail.com>
2024-02-20 09:53:45 +0700

    update view

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,10 +1,7 @@
- <!--   Core JS Files   -->
- <script src="<?= asset('js/core/popper.min.js') ?>"></script>
- <script src="<?= asset('js/core/bootstrap.min.js') ?>"></script>
- <script src="<?= asset('js/plugins/perfect-scrollbar.min.js') ?>"></script>
- <script src="<?= asset('js/plugins/smooth-scrollbar.min.js') ?>"></script>
- <script src="<?= asset('js/plugins/chartjs.min.js') ?>"></script>
- <!-- Github buttons -->
- <script async defer src="https://buttons.github.io/buttons.js"></script>
- <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
- <script src="<?= asset('js/argon-dashboard.min.js?v=2.0.4') ?>"></script>
+<script src="<?= asset('js/core/popper.min.js') ?>"></script>
+<script src="<?= asset('js/core/bootstrap.min.js') ?>"></script>
+<script src="<?= asset('js/plugins/perfect-scrollbar.min.js') ?>"></script>
+<script src="<?= asset('js/plugins/smooth-scrollbar.min.js') ?>"></script>
+<script src="<?= asset('js/plugins/chartjs.min.js') ?>"></script>
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<script src="<?= asset('js/argon-dashboard.min.js?v=2.0.4') ?>"></script>
--- app/views/templates/layout.php
+++ app/views/templates/layout.php
@@ -5,16 +5,79 @@
     <?= @$header ?>
 </head>
 
-<body class="g-sidenav-show bg-gray-100">
+<body class="g-sidenav-show   bg-gray-100">
     <div class="min-height-300 bg-primary position-absolute w-100"></div>
-    <?= @$sidebar ?>
+    <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+        <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+            <a class="navbar-brand m-0" href="" target="_blank">
+                <img src="<?= asset('img/logo-ct-dark.png') ?>" class="navbar-brand-img h-100" alt="main_logo">
+                <span class="ms-1 font-weight-bold">BAHASA ARAB</span>
+            </a>
+        </div>
+        <hr class="horizontal dark mt-0">
+        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+            <?= @$sidebar ?>
+        </div>
+
+    </aside>
     <main class="main-content position-relative border-radius-lg ">
+
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+                        <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+                    </ol>
+                    <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+                </nav>
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+
+                    </div>
+                    <ul class="navbar-nav  justify-content-end">
+                        <li class="nav-item d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+                                <i class="fa fa-user me-sm-1"></i>
+                                <span class="d-sm-inline d-none">Sign In</span>
+                            </a>
+                        </li>
+                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                                <div class="sidenav-toggler-inner">
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                    <i class="sidenav-toggler-line bg-white"></i>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
         <div class="container-fluid py-4">
-            <?= @$content ?>
+        <?= @$content ?>
+            <footer class="footer pt-3  ">
+                <div class="container-fluid">
+                    <div class="row align-items-center justify-content-lg-between">
+                        <div class="col-lg-6 mb-lg-0 mb-4">
+                            <div class="copyright text-center text-sm text-muted text-lg-start">
+                                © <script>
+                                    document.write(new Date().getFullYear())
+                                </script>,
+                                made with <i class="fa fa-heart"></i> by
+                                <a href="" class="font-weight-bold" target="_blank">Gugun Rizal Nugraha</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </footer>
         </div>
     </main>
-
-    <?= @$footer ?>
+    <?= @$footer; ?>
 </body>
 
 </html>
